b58bb67e555c27da2ce108f683bcd5293d355c03
xiang-ya <davidhoneu@gmail.com>
2022-10-01 12:45:03 -0400

    first commit

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
 from transformers import pipeline
+from diffusers import StableDiffusionPipeline
 import torch
+import string
+from torch import autocast
+
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    access_token = "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"
+
+    model_id = "CompVis/stable-diffusion-v1-1"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(device)
+    model = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=access_token)
+    model = model.to(device)
+
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
+def inference(model_inputs: dict) -> dict:
     global model
 
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
     if prompt == None:
         return {'message': "No prompt provided"}
-    
+
     # Run the model
-    result = model(prompt)
+    with autocast("cuda"):
+        result = model(prompt, guidance_scale=7.5).images[0]
 
     # Return the results as a dictionary
     return result
--- download.py
+++ download.py
@@ -4,10 +4,15 @@
 # In this example: A Huggingface BERT model
 
 from transformers import pipeline
+from diffusers import StableDiffusionPipeline
+
 
 def download_model():
+    access_token = "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"
+
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=access_token)
+
 
 if __name__ == "__main__":
     download_model()
